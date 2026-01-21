922a2876ec40dddfb0d97dfb38cae032a58cd5e7
Shirley <shirleytjy.98@gmail.com>
2026-01-21 22:57:07 +0800

    added permissions for internet, added view binding and fragment navigation

--- android-app/android-app/app/build.gradle.kts
+++ android-app/android-app/app/build.gradle.kts
@@ -19,6 +19,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -43,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
