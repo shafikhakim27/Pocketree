3fa1a201ac1ef5e3ca821a7d90f3ac2bc40e4d7c
Shirley <shirleytjy.98@gmail.com>
2026-01-23 12:55:43 +0800

    base fragments added
    createuseractivity and loginactivity logic developed (not yet tested with backend psps)

--- android-app/android-app/app/src/main/java/com/pocketree/app/SettingsFragment.kt
+++ android-app/android-app/app/src/main/java/com/pocketree/app/SettingsFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.pocketree.app.databinding.FragmentRedeemBinding
+import com.pocketree.app.databinding.FragmentSettingsBinding
 
-class RedeemFragment: Fragment() {
-    private var _binding: FragmentRedeemBinding? = null
+class SettingsFragment: Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRedeemBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
--- android-app/android-app/app/src/main/java/com/pocketree/app/User.kt
+++ android-app/android-app/app/src/main/java/com/pocketree/app/User.kt
@@ -1,12 +1,9 @@
 package com.pocketree.app
 
-import kotlinx.datetime.LocalDateTime
-import com.pocketree.app.Task
-
 data class User (
     val id: Int,
     val username: String,
-    val passswordHash: String,
+    val passwordHash: String,
     val totalCoins: Int,
     val currentLevelId: Int,
     val lastLoginDate: String, //Gson unable to parse LocalDateTime
