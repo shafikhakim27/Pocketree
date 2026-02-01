fae5cd427fa818989c7cc454956c96bd1581fcea
Shirley <shirleytjy.98@gmail.com>
2026-02-01 13:08:43 +0800

    missed out TaskCompletionResultDto earlier sorry

---
+++ api/Pocketree.Api/Models/DTOs/TaskCompletionResultDto.cs
@@ -0,0 +1,12 @@
+namespace ADproject.Models.DTOs
+{
+    public class TaskCompletionResultDto
+    {
+        public bool success { get; set; }
+        public string Status { get; set; }
+        public bool LevelUp { get; set; }
+        public int NewCoins { get; set; }
+        public int NewLevel { get; set; }
+        public bool IsWithered { get; set; }
+    }
+}
