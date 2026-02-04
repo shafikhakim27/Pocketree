76e6f79986e884a92dbedeca3f19481bb0f39f3e
Shirley <shirleytjy.98@gmail.com>
2026-02-04 14:52:59 +0800

    gradle bug fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,4 +64,8 @@ dependencies {
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
