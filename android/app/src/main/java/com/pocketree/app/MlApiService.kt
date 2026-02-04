c6b616466b57c41f43bfcd659b9e6770ce9b1a44
Frescylia Frida <225494842+fresfrida@users.noreply.github.com>
2026-02-04 13:49:00 +0800

    MlApiService

---
+++ android/app/src/main/java/com/pocketree/app/MlApiService.kt
@@ -0,0 +1,18 @@
+package com.pocketree.app
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import com.google.gson.annotations.SerializedName
+
+interface MlApiService {
+    @Multipart
+    @POST("classify")
+    suspend fun verifyImage(
+        @Part file: MultipartBody.Part,
+        @Part("keyword") keyword: RequestBody
+    ): Response<VerificationResponse>
+}
